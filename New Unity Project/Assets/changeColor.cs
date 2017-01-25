95ec9bbbbb033e1935d02865f21388332074aaed
Katie Morley <kamytherabbit@gmail.com>
2017-01-25 10:50:16 -0700

    Added comments to changeColor script

--- New Unity Project/Assets/changeColor.cs
+++ New Unity Project/Assets/changeColor.cs
@@ -9,10 +9,13 @@ public class changeColor : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.G))
 			GetComponent<Renderer> ().material.color = Color.green;
 		print ("Changed color to red.");
+		// turns object white
 		if (Input.GetKeyDown (KeyCode.Backspace))
 			GetComponent<Renderer> ().material.color = Color.white;
+		// turns object black
 		if (Input.GetKeyDown (KeyCode.Space))
 			GetComponent<Renderer> ().material.color = Color.black;
+		// turns object yellow
 		if (Input.GetKeyDown (KeyCode.Return))
 			GetComponent<Renderer> ().material.color = Color.yellow;
 	}
