2844eb9674620c06ef0ebfb41371422923cd29f5
Katie Morley <kamytherabbit@gmail.com>
2017-01-25 10:40:20 -0700

    Added more colorchange code to colorChange

--- New Unity Project/Assets/changeColor.cs
+++ New Unity Project/Assets/changeColor.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class changeColor : MonoBehaviour {
 	void Update() {
-		if (Input.GetKeyDown(KeyCode.R))
-			GetComponent<Renderer>().material.color = Color.red;
-		if (Input.GetKeyDown(KeyCode.B))
-			GetComponent<Renderer>().material.color = Color.blue;
-		if (Input.GetKeyDown(KeyCode.G))
-			GetComponent<Renderer>().material.color = Color.green;
+		if (Input.GetKeyDown (KeyCode.R))
+			GetComponent<Renderer> ().material.color = Color.red;
+		if (Input.GetKeyDown (KeyCode.B))
+			GetComponent<Renderer> ().material.color = Color.blue;
+		if (Input.GetKeyDown (KeyCode.G))
+			GetComponent<Renderer> ().material.color = Color.green;
+		print ("Changed color to red.");
+		if (Input.GetKeyDown (KeyCode.Backspace))
+			GetComponent<Renderer> ().material.color = Color.white;
+		if (Input.GetKeyDown (KeyCode.Space))
+			GetComponent<Renderer> ().material.color = Color.black;
+		if (Input.GetKeyDown (KeyCode.Return))
+			GetComponent<Renderer> ().material.color = Color.yellow;
 	}
 }
