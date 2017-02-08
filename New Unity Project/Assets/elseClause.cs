747c131e9688bc0b11c28d3bcce65f89aa599b67
Katie Morley <kamytherabbit@gmail.com>
2017-02-08 10:49:19 -0700

    Added if statments and else clause

---
+++ New Unity Project/Assets/elseClause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elseClause : MonoBehaviour {
+
+	public string password;
+	public bool userAgreement;
+
+	// Use this for initialization
+	void Start () {
+		// && = and || = or
+		if (password == "OU812" && userAgreement) {
+			print ("Correct");
+		} 
+
+		else {
+			print ("Incorrect");
+		}
+			
+
+	}
+
+
+
+
+}
+// else clauses cannot exist without an if statment and is the opposite of an if statment
+/* elseif clause is impetween the if and else 
+ * do not copy and paste code!!! Never use the else if statment.
+ * if statments are only used when the variable is constant or with two opptions. 
+ * (if the variable have more then one possible
+ * out come then don't use an if statment)
+ */
