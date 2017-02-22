b9b1aed9e1952733bbbe4a3aec1b56dcd0718d51
Katie Morley <kamytherabbit@gmail.com>
2017-02-22 11:29:55 -0700

    Added more lists

--- New Unity Project/Assets/lists.cs
+++ New Unity Project/Assets/lists.cs
@@ -5,19 +5,31 @@ using System.Collections.Generic;
 
 public class lists : MonoBehaviour {
 
-
+	public List<string> myFoodList;
+	public List<string> myHouseList;
 	public List<string> myWeaponList;
 	//lists are not a function it's an object.
 	public string[] myWeaponArray;
 
-	/*// Use this for initialization
+	// Use this for initialization
 	void Start () {
 		myWeaponList.Add ("Sword");
 		//myWeaponList.Remove ("Ax");
 		myWeaponList.Add ("Bow");
 		//myWeaponList.RemoveAt (1);
-	
-	}*/
+
+
+		myFoodList.Add("Sushi");
+		myFoodList.Add ("Plum");
+		myFoodList.Remove ("Tomato");
+
+		myHouseList.Add ("Table");
+		myHouseList.Add ("Kitchen");
+		myHouseList.Add ("Chair");
+		myHouseList.Add ("Bathroom");
+	}
+
+
 	public void AddToList(){
 		myWeaponList.Add ("Stick");	
 	}
