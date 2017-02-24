c9cce0bfbadb8887dc592b833e52badcf0e9b93a
Katie Morley <kamytherabbit@gmail.com>
2017-02-24 16:45:28 -0700

    Added more for loops

--- New Unity Project/Assets/ForLoops.cs
+++ New Unity Project/Assets/ForLoops.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ForLoops : MonoBehaviour {
 
 	public List<string> cars;
+	public List<string> music;
+	public List<string> food;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +15,16 @@ public class ForLoops : MonoBehaviour {
 			print (cars [i]);
 		}
 
-
+		for (int i = 0; i < music.Count; i++) {
+		
+			print (music [i]);
+		}
+		for (int i = 0; i < food.Count; i++) {
+		
+			print (food [i]);
+		
+		}
+	
 	}
 
 }
