69c62db07252f8ea942a852861db4e4c79b935ba
Katie Morley <kamytherabbit@gmail.com>
2017-03-03 11:09:04 -0700

    Added Logical Operators

---
+++ New Unity Project/Assets/Foreach.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foreach : MonoBehaviour {
+
+	public List <GameObject> powerUPList;
+	public List <GameObject> cubelist;
+	public List <GameObject> spherelist;
+	public List <GameObject> capsulelist;
+
+	// Use this for initialization
+	public void CheckData () {
+	
+		foreach (GameObject item in powerUPList) 
+		{
+			print (item);
+
+
+			switch (item.name) 
+			{
+
+			case "Cube":
+				cubelist.Add (item);
+				break;
+			case "Sphere":
+				spherelist.Add (item);
+				break;
+			case "Capsule":
+				capsulelist.Add (item);
+				break;
+			}
+
+		}
+
+	}
+}
---
+++ New Unity Project/Assets/LogicalOperators.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalOperators : MonoBehaviour {
+
+
+	public string string1;
+	public string string2;
+	public string Cat;
+	public string Dog;
+	public string Dessert;
+	public string Topping;
+
+
+	public void OnButtonClick () {
+	
+		if (string1 == "Thing1" && string2 == "Thing2") 
+		{
+		
+			print ("These things are quite tame. Oh so tame.");
+		
+		}
+
+		if (string1 == "Thing1" || string2 == "Thing2") 
+		{
+
+			print ("It's fun to have fun if you know what to do.");
+		
+		}
+
+		if (string1 != "Thing1" && string2 != "Thing2") 
+		{
+
+			print ("Oh what a shame, what a shame, what a shame.");
+		
+		}
+	
+		if (Cat == "Meow" && Dog == "Woof") {
+		
+			print ("There are two animals!"); 
+		}
+	
+		if (Cat == "Meow" || Dog == "Woof") {
+		
+			print ("One maybe two animals here");
+		}
+
+		if (Cat != "Meow" && Dog != "Woof") {
+			print ("What animals are these?!");
+		}
+
+		if (Dessert == "Brownie" && Topping == "Ice Cream") {
+			print ("This is Delicious!!");
+		}
+		if (Dessert == "Brownie" || Topping == "Ice Cream"){
+			print ("Either one is good!");
+		}
+		if (Dessert != "Brownie" && Topping != "Ice Cream") {
+		
+			print ("Where's my brownie sunday!");
+		}
+		if (Dessert == "Carrots" && Topping == "PeanutButter") {
+			print ("What???");
+		
+		}
+	}
+
+}
