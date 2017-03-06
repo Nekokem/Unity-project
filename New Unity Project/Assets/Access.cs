4f40d822f581eeaea4402b94959f4fe0ceeb2b9a
Katie Morley <kamytherabbit@gmail.com>
2017-03-06 10:53:10 -0700

    Added scope and access

---
+++ New Unity Project/Assets/Access.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Access : MonoBehaviour {
+
+	public Scope home;
+
+	// Use this for initialization
+	void Start () {
+		print (home.owner);
+		home.Awake ();
+	}
+	
+
+}
---
+++ New Unity Project/Assets/Scope.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scope : MonoBehaviour {
+
+	public string owner = "Bob";//scope lives in the class it's a class member
+	private string renter = "Kate";
+
+	public void Awake()
+	{
+		string cat = "Joe";//can only be seen in Awake
+
+		if (cat == "Joe") 
+		{
+			string mouse = "Helen";
+			print (cat + " ate " + mouse);
+			print (renter);
+		}
+		print (renter);
+		print (cat);
+		print (owner);
+	}
+
+
+	void Start () 
+	{
+		string cat = "Frank";
+		print (cat);
+		print (owner);
+	}
+	
+}
