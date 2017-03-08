be81e2af9e7d35595cd8e7bc56bd5a9b32b57c49
Katie Morley <kamytherabbit@gmail.com>
2017-03-08 09:24:04 -0700

    Added more to scope

--- New Unity Project/Assets/Scope.cs
+++ New Unity Project/Assets/Scope.cs
@@ -6,16 +6,23 @@ public class Scope : MonoBehaviour {
 
 	public string owner = "Bob";//scope lives in the class it's a class member
 	private string renter = "Kate";
+	public string renterDog= "Sugar";
+	private string renterChild = "Chris";
 
 	public void Awake()
 	{
 		string cat = "Joe";//can only be seen in Awake
-
+		string food = "Tuna";
 		if (cat == "Joe") 
 		{
 			string mouse = "Helen";
 			print (cat + " ate " + mouse);
 			print (renter);
+		}
+		if (food == "Tuna") 
+		{
+			string mouseFood = "Cheese";
+			print (cat + " is distracted by " + food);
 		}
 		print (renter);
 		print (cat);
@@ -26,6 +33,9 @@ public class Scope : MonoBehaviour {
 	void Start () 
 	{
 		string cat = "Frank";
+		if (renterDog == "Sugar") {
+			print (cat + " is scared of " + renterDog);
+		}
 		print (cat);
 		print (owner);
 	}
