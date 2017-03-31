ce712fb8eec183be6720ca88173c6ad2b51e74d1
Katie Morley <kamytherabbit@gmail.com>
2017-03-31 10:58:22 -0600

    Added VendingMechine chips and coke

---
+++ New Unity Project/Assets/MoneyInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyInput : MonoBehaviour {
+
+
+	public InputField money;
+	public float MoneyToFloat () 
+	{
+
+		return float.Parse(money.text);
+
+	}
+	//parse means to change
+
+}
---
+++ New Unity Project/Assets/VendingMachine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VendingMachine : MonoBehaviour {
+
+	public MoneyInput moneyInput;
+
+	public string productName = "Coke";
+	public float productPrice = 1.50f;
+
+
+	float VentProduct (float money) 
+	{
+		float change = 0;
+
+		if (money >= productPrice) 
+		{
+		
+			print ("Vending " + productName + ".");
+			change = money - productPrice;
+
+		}
+			
+
+		return change;
+
+	}
+
+
+	public void ButtonClick () 
+	{
+	
+		print ("$" + VentProduct (moneyInput.MoneyToFloat()) + " is your change");
+	
+
+	}
+}
